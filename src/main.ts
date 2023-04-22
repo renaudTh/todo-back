e543bb52b6859ae40d927f4687a3e4655af5271e
Thomas Renaud <t.renaud@neuf.fr>
2023-04-22 22:36:49 +0200

    Start Dockerization

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
   app.enableCors();
-  await app.listen(3000);
+  await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
